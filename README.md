b152bbad16a2dde5b39ba89ecd997b8ee9df2f33
Travis Kent Beste <travis@tencorners.com>
2022-12-18 15:48:22 -0600

    [WS-1] - forgot DTO members and readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# api-service
+
+
+# Methods
+
+1. /api/v1/category
--- src/main/java/com/walletsquire/apiservice/dtos/CategoryDTO.java
+++ src/main/java/com/walletsquire/apiservice/dtos/CategoryDTO.java
@@ -1,13 +1,17 @@
 package com.walletsquire.apiservice.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -15,7 +19,13 @@ import javax.validation.constraints.Size;
 public class CategoryDTO {
 
     private Long id;
+    @Size(min = 3, max = 64, message = "must be between 3 and 64 characters")
     private String name;
+
+    @Min(value = 1, message = "should be greater than 1")
     private Long parent;
+
+    @JsonIgnore
+    private Set<CategoryDTO> children = new HashSet<>();
 
 }
